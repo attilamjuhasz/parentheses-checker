336644c14e26dcb2d25d08041ae9d39a5e0c9a11
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2025-11-10 16:04:30 +0000

    Progress

--- PC.java
+++ PC.java
@@ -1,13 +1,32 @@
 public class PC{
     public static void main(String[] args){
-
+        System.out.println(
+            parantheses("()")
+            );
     }
 
-    public static void p(String str){
-        char curlOne = "(";
-        char curlTwo = ")";
-        for ()
+
+    public static String parantheses(String str){
+        char arcOne = '(';
+        char arcTwo = ')';
+        char curlOne = '{';
+        char curlTwo = '}';
+        char sqOne = '[';
+        char sqTwo = ']';
+        String end = "";
+
+        while (str.isEmpty() == false){
+            if (str.charAt(0) == arcOne || str.charAt(0) == curlOne || str.charAt(0) == sqOne){
+
+            }
+            else{
+                end = "Not Valid";
+                break;
+            }
+        }
+        return end;
     }
+    
 
 
 }
